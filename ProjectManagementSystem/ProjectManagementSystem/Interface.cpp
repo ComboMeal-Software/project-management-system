44f7f7f7fba1cf9839fcb41483d6ef92763cce8d
Thomas Miles <Dmilya2000@gmail.com>
2019-12-23 14:30:06 +0300

    Готовлюсь к merge от мастера

--- ProjectManagementSystem/ProjectManagementSystem/Interface.cpp
+++ ProjectManagementSystem/ProjectManagementSystem/Interface.cpp
@@ -1,7 +1,31 @@
-#include "Interface.h"
+﻿#include "Interface.h"
 
 #include "UserClass.h"
 #include "ProjectClass.h"
+#include "Database.h"
+
+Interface::Interface()
+{
+	database = new Database();
+	currentUser = NULL;
+	commands = {
+		{"start", {"register [name] [password]", "login [name] [password"}},
+		{"logged_in",{"create_project", "my_projects", "edit_info", "check_notifications"}}
+	};
+}
+
+void Interface::createProject()
+{
+	std::string name;
+	std::string objective;
+	std::string tasks;
+	std::string subjectField;
+	std::string client;
+	std::string deadline;
+	std::string prerequisites;
+	std::cout << "Введите информацию о создаваемом проекте. В дальнейшем информация может быть изменена.";
+	std::cout << "Back - выйти";
+}
 
 Interface::~Interface()
 {
